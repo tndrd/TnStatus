204c136f9149dfd6217a5f3cf5cd4f9d49042856
Vladimir Lekhterev <vovalekhterev@gmail.com>
2023-11-09 18:13:47 +0300

    Create library

---
+++ Inc/TnStatus.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+  #define TN_STATUS_DECLARE(code, description) code,
+  #include "TnStatusCode.decl"
+  #undef TN_STATUS_DECLARE
+} TnStatusCode;
+
+typedef struct {
+  TnStatusCode Code;
+  
+  #ifdef TN_STATUS_EXTENDED
+  size_t Line;
+  const char* Function;
+  const char* File;
+  const char* Comment;
+  #endif
+} TnStatus;
+
+#ifdef TN_STATUS_EXTENDED
+TnStatus CreateStatus(TnStatusCode code, size_t line, const char* function, const char* file, const char* comment);
+#define TNSTATUS_C(code, comment) CreateStatus(code, __LINE__, __FUNCTION__, __FILE__, comment);
+#else
+TnStatus CreateStatus(TnStatusCode code);
+#define TNSTATUS_C(code, comment) CreateStatus(code);
+#endif
+
+#define TNSTATUS(code) TNSTATUS_C(code, NULL)
+
+const char* TnStatusCodeGetDescription(TnStatusCode code);
+void TnStatusPrintDescription(TnStatus status);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ Src/TnStatus.c
@@ -0,0 +1,33 @@
+#include "TnStatus.h"
+
+#ifdef TN_STATUS_EXTENDED
+TnStatus CreateStatus(TnStatusCode code, size_t line, const char* function, const char* file, const char* comment) {
+  TnStatus status = {code, line, function, file, comment};
+  return status;
+} 
+#else
+TnStatus CreateStatus(TnStatusCode code) {
+  TnStatus status = {code};
+  return status;
+}
+#endif
+
+const char* TnStatusCodeGetDescription(TnStatusCode code) {
+  switch(code) {
+    #define TN_STATUS_DECLARE(code, description) case code: return #description;
+    #include "TnStatusCode.decl"
+    #undef TN_STATUS_DECLARE
+
+    default: return "Undefined status code";
+  }
+}
+
+void TnStatusPrintDescription(TnStatus status) {
+  #ifdef TN_STATUS_EXTENDED
+  fprintf(stderr, "%s, %s, %zu: ", status.File, status.Function, status.Line);
+  if (status.Comment)
+    fprintf(file, "%s: ", status.Comment);
+  #endif
+
+  fprintf(stderr, "%s", TnStatusCodeGetDescription(status.Code));
+}
